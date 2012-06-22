8ecdd8565babe65992caff6374c39f77334901c7
Akhil Vij <akhil.iiit@gmail.com>
2012-06-22 15:50:26 +0530

    Added code and mappings for identifying Required-Start and Should-Start headers.

--- converter.py
+++ converter.py
@@ -1,5 +1,6 @@
 
-import ConfigParser, sys
+import ConfigParser
+import sys
 
 config = ConfigParser.ConfigParser()
 
@@ -11,114 +12,188 @@ else:
 
 def add_description():
 	if check_for("Unit", "Description"):
-		print "Short-Description: "+ config.get("Unit", "Description")
+		print "Short-Description: " + config.get("Unit", "Description")
 	
 def add_runlevels():
 	if check_for("Install", "WantedBy"):
 		runlevel = config.get("Install", "WantedBy")
 		if runlevel == "multi-user.target":
 			print "Default-Start:\t2 3 4"
-			print "Default-Stop:"
+			print "Default-Stop:\t0 1 6"
+			return 4
 		
 		elif runlevel == "graphical.target":
 			print "Default-Start:\t2 3 4 5"
-			print "Default-Stop:\t??"
+			print "Default-Stop:\t0 1 6??"
+			return 5
 		
 		#Not sure about basic.target & rescue.target
 		elif runlevel == "basic.target":
 			print "Default-Start:\t1"
-			print "Default-Stop:\t??"	
+			print "Default-Stop:\t??"
+			return 1	
 			
 		elif runlevel == "rescue.target":
 			print "Default-Start:\t1"
+			return 1
 			
 		else:
 			return
-			
+	
 def add_required_service():
-	if check_for("Unit", "After"):
-		after_services_str = config.get("Unit", "After")
-		required_str = "Required-Start:\t"
-		for service in  after_services_str.split(" "):
-			if service == "syslog.target":
-				required_str = required_str + "$syslog "
-			elif service == "remote-fs.target":
-				required_str = required_str + "$remote_fs "
-			elif service == "network.target":
-				required_str = required_str + "$network "
-			else:
-				break
-			
-		print required_str
+	
+	required_str = "Required-Start:\t"
+	remote_fs_flag = True
+	syslog_flag = True
+	network_flag = True
+	local_fs_flag = True
+	rpcbind_flag = True
+	options = ['After', 'Requires']
+	
+	for option in options:
+		if check_for("Unit", option):
+			after_services_str = config.get("Unit", option)
+			for unit in  after_services_str.split(" "):
+				if unit == "syslog.target" and syslog_flag:
+					required_str = required_str + "$syslog "
+					syslog_flag = False
+				elif (unit == "remote-fs.target" or unit == 
+					"proc-fs-nfsd.mount" or unit == 
+					"var-lib-nfs-rpc_pipefs.mount") and remote_fs_flag:
+					required_str = required_str + "$remote_fs "
+					remote_fs_flag = False
+				elif unit == "network.target" and network_flag:
+					required_str = required_str + "$network "
+					network_flag = False
+				elif unit == "local_fs.target" and local_fs_flag:
+					required_str = required_str + "$local_fs "
+					local_fs_flag = False
+				elif unit == "rpcbind.service" and rpcbind_flag:
+					required_str = required_str + "$portmap"
+					rpcbind_flag = False
+				else:
+					break	
+				
+	print required_str
+	
+def add_should_service():
+	
+	should_str = "Should-Start:\t"
+	remote_fs_flag = True
+	syslog_flag = True
+	network_flag = True
+	local_fs_flag = True
+	rpcbind_flag = True
+	options = ['Wants']
+	
+	for option in options:
+		if check_for("Unit", option):
+			after_services_str = config.get("Unit", option)
+			for unit in  after_services_str.split(" "):
+				if unit == "syslog.target" and syslog_flag:
+					should_str = should_str + "$syslog "
+					syslog_flag = False
+				elif (unit == "remote-fs.target" or unit == 
+					"proc-fs-nfsd.mount" or unit == 
+					"var-lib-nfs-rpc_pipefs.mount") and remote_fs_flag:
+					should_str = should_str + "$remote_fs "
+					remote_fs_flag = False
+				elif unit == "network.target" and network_flag:
+					should_str = should_str + "$network "
+					network_flag = False
+				elif unit == "local_fs.target" and local_fs_flag:
+					should_str = should_str + "$local_fs "
+					local_fs_flag = False
+				elif unit == "rpcbind.service" and rpcbind_flag:
+					should_str = should_str + "$portmap"
+					rpcbind_flag = False
+				else:
+					break	
+				
+	print should_str
 
 def check_env_file(Environment_file):
+	
 	print "if test -f " + Environment_file + "; then\n\t. " + Environment_file + "\nfi\n"
 	
 def check_for(service, option):	
+	
 	try:
-		config.get(service,option)
+		config.get(service, option)
 		return 1
 	except:
 		return 0
 	
 def build_LSB_header(): #add more arguments here
+	
 	print "### BEGIN INIT INFO"
-# Call functions here for Provides, Required-Start, Required-Stop, Default-Start, Default-Stop, Short-Description and Description
-# Don't know whether we can get all the info for this from the "Unit" Section alone....check this...
+# Call functions here for Provides, Required-Start, Required-Stop, 
+# Default-Start, Default-Stop, Short-Description and Description. Don't know 
+# whether we can get all the info for this from the "Unit" Section alone.
+
 	add_description()
 	add_required_service()
+	add_should_service()
 	add_runlevels()
 	print "### END INIT INFO"
 
 def build_start():
+	
 	print "start() {\necho -n \"Starting $prog: \""
-# Call functions here to check for ExecStartPre, post and all other options
-	if check_for("Service","ExecStartPre"):
+
+	if check_for("Service", "ExecStartPre"):
 		print config.get("Service", "ExecStartPre")
 	
-	print config.get("Service", "ExecStart")
+	if check_for("Service", "ExecStart"):
+		print config.get("Service", "ExecStart")
 	
-	if check_for("Service","ExecStartPost"):
+	if check_for("Service", "ExecStartPost"):
 		print config.get("Service", "ExecStartPost")
 	
 	print "}\n"
 
 def build_stop():
-	print "stop() {\necho -n \"Stopping $prog: \""
-# Call functions here to check for options
-	print config.get("Service", "ExecStop")
 	
-	if check_for("Service","ExecStopPost"):
+	print "stop() {\necho -n \"Stopping $prog: \""
+
+	if check_for("Service", "ExecStop"):
+		print config.get("Service", "ExecStop")
+	
+	if check_for("Service", "ExecStopPost"):
 		print config.get("Service", "ExecStopPost")
 		
 	print "}\n"
 	
 def build_reload():
+	
 	print "reload () {\necho -n \"Reloading $prog: \""
-	print config.get("Service", "ExecReload")
+	if check_for("Service", "ExecReload"):
+		print config.get("Service", "ExecReload")
 	print "}\n"
 
-# For environment, PID, run files etc.
 def build_default_params():
+	
 	print "\nset -e\n"
-	check_env_file(config.get("Service","EnvironmentFile"));
 	print "prog=" + prog
+	
+	if check_for("Service", "EnvironmentFile"):
+		check_env_file(config.get("Service", "EnvironmentFile"));
+	
 	if check_for("Service", "PIDFile"):
 		print "pidfile=${PIDFILE-" + config.get("Service", "PIDFile")
+	
 	print
 
-# Similarly we would have build_restart(), build_reload(), build_status() and so on...
-
 for section in config.sections():
-		if section == "Unit":
-			build_LSB_header();
-
-		if section == "Service":		
+	if section == "Unit":
+		build_LSB_header();
+	if section == "Service":		
 			build_default_params()
-			for option in config.options(section):
-				if option == "execstart":
-					build_start()
-				elif option == "execstop":
-					build_stop()
-				elif option == "execreload":
-					build_reload()
+
+# The build_{start,stop,reload} functions will be called irrespective of the 
+# existence of Exec{Start,Stop,Reload} options. This is to ensure that all the
+# basic call exists(even if they have no operation).
+
+build_start()
+build_stop()
+build_reload()
